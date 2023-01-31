268070998d83eb7fbdfb8faef4c28a7965abf465
cray-sh <palenchara@gmail.com>
2023-01-30 20:04:56 -0500

    Update Problem3 Playground
    This adds examples for tables, pie graphs, and adding text to pdf documents.

--- problem 3/problem3_workingground.py
+++ problem 3/problem3_workingground.py
@@ -46,7 +46,7 @@ message.set_content(body)
 
 #Below will be setting up to attach a file
 
-attach_path = ""
+attach_path = "C:\\Users\\ADP55\\Desktop\\testfolder\\gobble.jpg"
 
 #Below sets up a way for us to get the mimetype and subtype that will need to be set
 #before we can send the message
@@ -103,8 +103,68 @@ mail_server.quit()
 
 #%% Block 2 - Generate PDF Simple Example
 
+#I'm going to group all of the import lines together here
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, Image   #these are the flowables
+from reportlab.lib.styles import getSampleStyleSheet                                #this is used for style references for text
+from reportlab.lib import colors                                                    #this is used for advanced table design
+from reportlab.graphics.shapes import Drawing                                       #this is used for drawing, along with Pie make Pie graphs
+from reportlab.graphics.charts.piecharts import Pie                                 #this is used for Pie, along with drawing make Pie graphs
+from reportlab.lib.units import inch, cm                                            #this is used for assigning the converted values for inch and cm
 
 
+#below will be variables needed for later
+
+table_data = []
+
+#this is what creates the blank pdf to build on, you can even give it an abs path + name to be more specific
+report = SimpleDocTemplate("C:\\Users\\ADP55\\Desktop\\Master Scripts\\testfolder\\Testreport.pdf")
+styles = getSampleStyleSheet()
+
+fruit = {
+"elder" : 1,
+"apples" : 2,
+"strawb" : 4,
+"pineap" : 4,
+"durian" : 5,
+"bluebur": 3
+}
+
+#Creating flowables/chunks:
+
+#Text
+report_title = Paragraph("This is an Inventory of Available Fruit", styles["h1"])
+
+#Table conversion from dictionary, tables want a list of lists aka 2d array
+for k,v in fruit.items():
+    table_data.append([k,v])
+
+#Basic Table
+report_table = Table(data=table_data)
+
+#Adv Table
+table_style = [('GRID', (0,0), (-1,-1), 1, colors.black)]
+
+report_table2 = Table(data=table_data, style=table_style, hAlign="LEFT")
+
+#Pie Graph
+#creates a blank pie graph, needs data and labels
+report_pie = Pie(width=3*inch, height=3*inch)
+
+report_pie.data = []
+report_pie.labels = []
+
+#sorts data from dictionary fruit into separate lists for data and labels
+for fruit_name in sorted(fruit):
+    report_pie.data.append(fruit[fruit_name])
+    report_pie.labels.append(fruit_name)
+
+#we need to first add a blank drawing (might be considered a flowable) then add our pie graph to it    
+report_chart = Drawing()
+report_chart.add(report_pie)
+
+#Below here will be the build function that requires all flowables  passed to it in a list
+
+report.build([report_title, report_table2, report_chart])
 
 
 #%% Block 3 - Simple bring it all together Example
